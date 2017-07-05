ae183003a566fbb58fbb65d02db03645e4dc0656
Jessica Palacios <jesspalacios@Jessicas-MacBook.local>
2017-07-05 19:04:12 -0400

    4th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,31 @@ import { Component } from '@angular/core';
     
   `],
   template: `
+  <header>
+  <nav class="navbar navbar-inverse">
+  <div class="navbar-header">
+    <a href="/" class="navbar-brand">My Angular App!</a>
+    </div>
+    </nav>
+    </header>
+
     <div class="jumbotron text-center">
       <h1>The App Lives!</h1>
       <p>{{ message }}</p>
     </div>
+
+    <p> The user is {{ user.name }} also known as {{ user.username }}. 😝</p>
+
+    <footer class="text-center">
+    CopyRight &copy; 2016
+    </footer>
   `
 })
 export class AppComponent {
-  message = 'This is the sample message.';
+  message = 'Hey its a LIVE!.';
+  user= {
+    id: 25,
+    name: 'Jess',
+    username: 'mz_lovelyJ'
+  }
 }

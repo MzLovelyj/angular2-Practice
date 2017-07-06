c43f7ccfd5a52df2d38f607ebe4368e72a0fd468
Jessica Palacios <jesspalacios@jessicasmacbook.attlocal.net>
2017-07-05 22:44:32 -0400

    4th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,35 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styles: [`
-    
-  `],
-  template: `
-  <header>
-  <nav class="navbar navbar-inverse">
-  <div class="navbar-header">
-    <a href="/" class="navbar-brand">My Angular App!</a>
-    </div>
-    </nav>
+   template: `
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="navbar-header">
+          <a href="/" class="navbar-brand">Its Alive!</a>
+        </div>
+      </nav>
     </header>
-
-    <div class="jumbotron text-center">
-      <h1>The App Lives!</h1>
-      <p>{{ message }}</p>
-    </div>
-
-    <p> The user is {{ user.name }} also known as {{ user.username }}. 😝</p>
-
+    <main>
+      <div class="jumbotron">
+        <h1>Welcome to Our App!</h1>
+        <p>{{ message }}</p>
+      </div>
+      <div *ngIf="users">
+      <div *ngFor="let user of users">
+      {{ user.name }} Is also known as UserName: {{ user.username }}
+      </div>
+      </div>
+    </main>
     <footer class="text-center">
-    CopyRight &copy; 2016
+      Copyright &copy; 2016
     </footer>
-  `
+  `,
+  styles: [`
+    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
+  `]
 })
 export class AppComponent {
-  message = 'Hey its a LIVE!.';
-  user= {
-    id: 25,
-    name: 'Jess',
-    username: 'mz_lovelyJ'
-  }
+  message = 'Hello!';
+  users = [
+    { id: 25, name: 'Jess', username: 'mz_lovelyJ' },
+    { id: 26, name: 'Stephanie', username: 'Steph' },
+    { id: 27, name: 'Isabella', username: 'Izi' }
+  ];
 }
